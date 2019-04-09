ed8217fcfa8a74566324b0aa93e1bd2c9b25e68e
Danilo Salvati <danilo.salvati@blastingnews.com>
2019-04-09 16:13:15 +0200

    Added components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CategoriesComponent} from './categories/categories.component';
+import {CategoryDetailComponent} from './category-detail/category-detail.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CategoriesComponent},
+  {path: 'categories', redirectTo: '/'},
+  {path: 'category/:id', component: CategoryDetailComponent},
+  {path: 'product/:id', component: ProductDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    CategoryDetailComponent,
+    ProductDetailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
