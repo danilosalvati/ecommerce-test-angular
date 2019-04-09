7c718852a4b102a0ce89eae19610fafbc5467f4f
Danilo Salvati <danilo.salvati@blastingnews.com>
2019-04-09 16:15:53 +0200

    App refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {CategoriesComponent} from './categories/categories.component';
-import {CategoryDetailComponent} from './category-detail/category-detail.component';
-import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {CategoriesComponent} from './components/categories/categories.component';
+import {CategoryDetailComponent} from './components/category-detail/category-detail.component';
+import {ProductDetailComponent} from './components/product-detail/product-detail.component';
 
 const routes: Routes = [
   {path: '', component: CategoriesComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { CategoryDetailComponent } from './category-detail/category-detail.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CategoryDetailComponent } from './components/category-detail/category-detail.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
