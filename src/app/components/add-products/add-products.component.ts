680cbf336d7679aa1a65859cf94184996e878c3f
Rosario <rosario.russo@blastingnews.com>
2019-04-12 11:40:48 +0200

    Added add-products component

---
+++ src/app/components/add-products/add-products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-products',
+  templateUrl: './add-products.component.html',
+  styleUrls: ['./add-products.component.scss']
+})
+export class AddProductsComponent implements OnInit {
+
+  constructor() { }
+
+  addProduct(){
+
+  }
+
+  ngOnInit() {
+  }
+
+}
